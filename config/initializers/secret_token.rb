d5ce52631b3a12ce2bc52e91f0c5cf886a1265ad
devendra <devendrabobde89@gmail.com>
2012-08-17 23:27:00 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VinsolListApp::Application.config.secret_token = '11e73e4a53c074b0770701c7faa91f1657999a4df10de0537cb8f275dd4e99e14784ed8eb57eb17a12fcd082a50c07a11e5255a99f56b355a7db6ff2dba5545f'
